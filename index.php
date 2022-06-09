8198800f6dea82ef783a83f3e0eb868f7f99c389
John Montgomery <88300340+j-ace-svg@users.noreply.github.com>
2022-06-09 21:03:18 +0000

    Cleared page to prep for practice project

--- index.php
+++ index.php
@@ -3,15 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="styles.css">
   </head>
   <body>
-
-    <p>This is a paragraph!</p>
-    <?php if (true) { ?>
-      echo "This is also a paragraph!";
-    <?php } ?>
-    <p>This is a paragraph!</p>
+    
     
   </body>
 </html>
