f251136e64e74b20eb20b7b840ce0c294ae03c0d
John Montgomery <88300340+j-ace-svg@users.noreply.github.com>
2022-06-12 19:17:16 +0000

    Add calculator

--- index.php
+++ index.php
@@ -5,7 +5,17 @@
     <title></title>
   </head>
   <body>
-    
+
+    <form action="functions.php" metho="get">
+      <input type="text" name="num01" placeholder="Number 1">
+      <select name="oper">
+        <label>Choose operation!</label>
+        <option value="add">Add!</option>
+        <option value="sub">Subtract!</option>
+      </select>
+      <input type="text" name="num02" placeholder="Number 2">
+      <button type="submit">Calculate!</button>
+    </form>
     
   </body>
 </html>
