4b7e131f2fa9fd0fdb15032ff82051e272f264b8
John Montgomery <j.ace.svg@gmail.com>
2022-07-03 10:30:57 -0500

    Pull improved autoloader from master branch

--- includes/class-autoload.inc.php
+++ includes/class-autoload.inc.php
@@ -3,7 +3,14 @@
 spl_autoload_register('myAutoLoader');
 
 function myAutoLoader($className) {
-  $path = "classes/";
+  $url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+  if (strpos($url, 'includes') !== false) {
+    $path = '../classes/';
+  }
+  else {
+    $path = "classes/";
+  }
   $extension = ".class.php";
   $fullPath = $path . $className . $extension;
   
